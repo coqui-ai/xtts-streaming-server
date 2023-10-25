bc7eedc0a5a541d3801818d046f3fe402e3081b1
Reuben Morais <reuben.morais@gmail.com>
2023-10-25 13:12:08 +0200

    Clean up test script and add requirements.txt

--- test/test_streaming.py
+++ test/test_streaming.py
@@ -1,14 +1,13 @@
 import argparse
+import json
 import shutil
 import subprocess
 import sys
 import time
-import json
-import requests
-import copy
-import os
-from io import BytesIO
 from typing import Iterator
+
+import requests
+
 
 def is_installed(lib_name: str) -> bool:
     lib = shutil.which(lib_name)
@@ -16,16 +15,18 @@ def is_installed(lib_name: str) -> bool:
         return False
     return True
 
+
 def save(audio: bytes, filename: str) -> None:
     with open(filename, "wb") as f:
         f.write(audio)
 
+
 def stream_ffplay(audio_stream, output_file, save=True):
     if not save:
-        ffplay_cmd = ["ffplay", "-autoexit", "-"]
+        ffplay_cmd = ["ffplay", "-nodisp", "-probesize", "1024", "-autoexit", "-"]
     else:
         print("Saving to ", output_file)
-        ffplay_cmd = ["ffmpeg", "-i", "-", output_file]
+        ffplay_cmd = ["ffmpeg", "-probesize", "1024", "-i", "-", output_file]
 
     ffplay_proc = subprocess.Popen(ffplay_cmd, stdin=subprocess.PIPE)
     for chunk in audio_stream:
@@ -36,32 +37,38 @@ def stream_ffplay(audio_stream, output_file, save=True):
     ffplay_proc.stdin.close()
     ffplay_proc.wait()
 
+
 def tts(text, speaker) -> Iterator[bytes]:
     start = time.perf_counter()
     speaker["text"] = text
     speaker["language"] = "en"
     res = requests.post(
-        "http://localhost:5000/predict_streaming/",
+        "http://localhost:8000/tts_stream",
         json=speaker,
         stream=True,
     )
     end = time.perf_counter()
     print(f"Time to make POST: {end-start}s", file=sys.stderr)
+
+    if res.status_code != 200:
+        print("Error:", res.text)
+        sys.exit(1)
+
     first = True
-    for chunk in res.iter_content(chunk_size=None):
+    for chunk in res.iter_content(chunk_size=512):
         if first:
             end = time.perf_counter()
             print(f"Time to first chunk: {end-start}s", file=sys.stderr)
             first = False
         if chunk:
-            print("chunk len:", len(chunk))
             yield chunk
 
     print("⏱️ response.elapsed:", res.elapsed)
 
+
 def get_speaker(ref_audio):
     files = {"wav_file": ("reference.wav", open(ref_audio, "rb"))}
-    response = requests.post(f"http://localhost:5000/predict_speaker", files=files)
+    response = requests.post("http://localhost:8000/clone_speaker", files=files)
     return response.json()
 
 
@@ -73,7 +80,7 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--output_file",
-        default="./output.mp3",
+        default=None,
     )
     parser.add_argument(
         "--ref_file",
@@ -81,11 +88,11 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    with open('./default_speaker.json', 'r') as file:
+    with open("./default_speaker.json", "r") as file:
         speaker = json.load(file)
-    
+
     if args.ref_file is not None:
         print("Computing the latents for a new reference...")
         speaker = get_speaker(args.ref_file)
 
-    audio = stream_ffplay(tts(args.text, speaker), args.output_file, save=True)
+    audio = stream_ffplay(tts(args.text, speaker), args.output_file, save=bool(args.output_file))
