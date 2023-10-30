8dfdbd6870a3d70d059be4b0594739cfd66db957
ggoknar <ggoknar@coqui.ai>
2023-10-30 14:53:20 +0300

    add some logs

--- server/main.py
+++ server/main.py
@@ -22,14 +22,18 @@ torch.set_num_threads(8)
 device = torch.device("cuda")
 
 model_name = "tts_models/multilingual/multi-dataset/xtts_v1.1"
+print("Downloading XTTS Model:",model_name)
 ModelManager().download_model(model_name)
 model_path = os.path.join(get_user_data_dir("tts"), model_name.replace("/", "--"))
+print("XTTS Model downloaded")
 
+print("Loading XTTS")
 config = XttsConfig()
 config.load_json(os.path.join(model_path, "config.json"))
 model = Xtts.init_from_config(config)
 model.load_checkpoint(config, checkpoint_dir=model_path, eval=True, use_deepspeed=True)
 model.to(device)
+print("XTTS Loaded.")
 
 ##### Run fastapi #####
 app = FastAPI(
